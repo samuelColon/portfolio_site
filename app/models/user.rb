e2777cafa2cd1be1ef0286a001138dd010d80221
samuelColon <samsoncs@gmail.com>
2015-09-21 15:51:27 -0400

    stash

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable, :registerable,
+  # I mean I should be the only user right?
+  if Rails.env.production?
+    devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+  else
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  end
 
   has_many :projects
 end
